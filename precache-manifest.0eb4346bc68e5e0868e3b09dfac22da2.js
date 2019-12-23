7730f5f8eab7edcb9a2c69a77a34de765a7e89a2
kanahan <kanahan0110@gmail.com>
2019-12-23 16:31:52 +0800

    css module

---
+++ precache-manifest.0eb4346bc68e5e0868e3b09dfac22da2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc2b9e70376549c0a189b8159c2977b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54f7bb14d3f203875e75",
+    "url": "/static/css/main.70773ef5.chunk.css"
+  },
+  {
+    "revision": "5139d4189fe3eeea94e4",
+    "url": "/static/js/2.22a5ed54.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.22a5ed54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54f7bb14d3f203875e75",
+    "url": "/static/js/main.40fc7985.chunk.js"
+  },
+  {
+    "revision": "6c7b059308995d673b3b",
+    "url": "/static/js/runtime-main.eae20a6d.js"
+  },
+  {
+    "revision": "405d30dfb88061f031a695b344221ba1",
+    "url": "/static/media/pic.405d30df.png"
+  }
+]);

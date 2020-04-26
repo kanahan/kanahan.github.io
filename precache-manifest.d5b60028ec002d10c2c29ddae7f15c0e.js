6d6d4e1ac623dc4a2256b93fd844e8936716826a
krsntn <kanahan0110@gmail.com>
2020-04-26 15:35:55 +0800

    update portfolio

---
+++ precache-manifest.d5b60028ec002d10c2c29ddae7f15c0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d6259ee3e1ab6a188c018853d89969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f44d102e52c6fd3853c",
+    "url": "/static/css/main.fba552a5.chunk.css"
+  },
+  {
+    "revision": "2d88b1a47b3929e16a3c",
+    "url": "/static/js/2.737ac857.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.737ac857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f44d102e52c6fd3853c",
+    "url": "/static/js/main.0cc4d19c.chunk.js"
+  },
+  {
+    "revision": "440d339005ed9692dda7",
+    "url": "/static/js/runtime-main.9b563960.js"
+  },
+  {
+    "revision": "405d30dfb88061f031a695b344221ba1",
+    "url": "/static/media/pic.405d30df.png"
+  }
+]);
